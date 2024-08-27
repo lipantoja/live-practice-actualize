7f776a502b7ee6b170cfe86489fede40916f8c0a
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-26 20:38:59 -0400

    First group code

---
+++ live-121.rb
@@ -0,0 +1,15 @@
+# 2) Write a function to print when a person has died in a sentence.
+
+dickens = ["Charles Dickens", "1870"]
+thackeray = ["William Thackeray", "1863"]
+trollope = ["Anthony Trollope", "1882"]
+hopkins = ["Gerard Manley Hopkins", "1889"]
+def died(array)
+  name = array[0]
+  year = array[1]
+  puts  "#{name} died in #{year}."
+end
+puts died dickens
+puts died(thackeray)
+puts died(trollope)
+puts died(hopkins)
